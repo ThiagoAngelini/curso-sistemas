0e6e6084b26394fbf1a0d3c6b681dd7c9a184226
Thiago Angelini <you@example.com>
2020-08-03 16:39:52 -0300

    Formulario POST

--- Tema 3 - Formularios  Protocolo HTTP/Formulario POST/form_post_server.php
+++ Tema 3 - Formularios  Protocolo HTTP/Formulario POST/form_post_server.php
@@ -7,6 +7,7 @@
     $_REQUEST   ['number'];
     $_REQUEST   ['email'];
     $_REQUEST   ['password'];
+    $_REQUEST   ['file'];
 
 ?>
 <!DOCTYPE html>
@@ -20,10 +21,12 @@
         <p><?php echo "Nombre: ".               $_REQUEST['name'] ?></p>
         <p><?php echo "Apellido: ".             $_REQUEST['surname'] ?></p>
         <p><?php echo "Genero: ".               $_REQUEST['gender'] ?></p>
-        <p><?php echo "Fecha de nacimiento: ".   $_REQUEST['birth'] ?></p>
+        <p><?php echo "Fecha de nacimiento: ".  $_REQUEST['birth'] ?></p>
         <p><?php echo "Pais: ".                 $_REQUEST['country'] ?></p>
         <p><?php echo "Codigo postal: ".        $_REQUEST['number'] ?></p>
         <p><?php echo "Correo electrónico: ".   $_REQUEST['email'] ?></p>
         <p><?php echo "Contraseña: ".           $_REQUEST['password'] ?></p>
+        <p><?php echo "Archivo: ".              $_REQUEST['file'] ?></p>
+       
     </body>
 </html>
